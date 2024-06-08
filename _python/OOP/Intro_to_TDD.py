2d9237e21a128e79f6aea01f6c38e8068886fae5
Deek <deek9517@gmail.com>
2024-06-08 14:09:00 +0300

    intro_to_tdd

--- _python/OOP/Intro_to_TDD.py
+++ _python/OOP/Intro_to_TDD.py
@@ -65,6 +65,116 @@ class is_PalindromeTest(unittest.TestCase):
         print("running tearDown tasks")
 
 
+3#
+def coin(amount):
+    coins = [0,0,0,0]
+    quarter = 25 
+    dime = 10 
+    nickel = 5 
+    pinnis = 1 
+    for i in range(amount):
+        if amount >= 25 :
+            coins[0] +=1
+            amount = amount -25
+        elif amount >=10 <25 :
+            coins[1] += 1 
+            amount = amount -10
+        elif amount >=5 <10 :
+            coins[2] += 1 
+            amount = amount -5
+        elif amount >=1 <5 :
+            coins[3] += 1 
+            amount = amount -1
+    return coins
+
+class coinTest(unittest.TestCase):
+    def testOne(self):
+        self.assertEqual(coin(87) , [3,1,0,2])
+
+    def testTwo(self):
+        self.assertEqual(coin(105) , [4,0,1,0])
+    
+    def testThree(self):
+        self.assertEqual(coin(207) , [8,0,1,2])
+
+    def testFour(self):
+        self.assertEqual(coin(156) , [6,0,1,1])
+
+    def testFive(self):
+        self.assertEqual(coin(452) , [18,0,0,2])
+
+    def testFive(self):
+        self.assertEqual(coin(54) , [2,0,0,4])
+
+    def setUp(self):
+
+        print("running setUp")
+
+    def tearDown(self):
+
+        print("running tearDown tasks")
+
+
+
+4#
+def factorial(num):
+    factor = 1
+    for i in range(num , 0 , -1 ):
+        factor = factor*i
+    return factor
+    
+class factorialTest(unittest.TestCase):
+    def testOne(self):
+        self.assertEqual(factorial(5) , 120)
+
+    def testTwo(self):
+        self.assertEqual(factorial(4) , 24)
+    
+    def testThree(self):
+        self.assertEqual(factorial(9) , 362880)
+
+    def testFour(self):
+        self.assertEqual(factorial(3) , 6)
+
+
+    def setUp(self):
+
+        print("running setUp")
+
+    def tearDown(self):
+
+        print("running tearDown tasks")
+
+5#
+
+def fib(num):
+    fibbonacci = [0 , 1]
+    for i in range(num-1):
+        sum = fibbonacci[i] + fibbonacci[i+1]
+        fibbonacci.append(sum)
+    return fibbonacci[num] 
+
+class fibbonacciTest(unittest.TestCase):
+    def testOne(self):
+        self.assertEqual(fib(5) , 5)
+
+    def testTwo(self):
+        self.assertEqual(fib(4) , 3)
+    
+    def testThree(self):
+        self.assertEqual(fib(6) , 8)
+
+    def testFour(self):
+        self.assertEqual(fib(7) , 13)
+
+
+    def setUp(self):
+
+        print("running setUp")
+
+    def tearDown(self):
+
+        print("running tearDown tasks")
 
 
 
