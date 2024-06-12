1f96a7e56967892aec893cea8dd6f7f6264e156e
Deek <deek9517@gmail.com>
2024-06-12 16:04:47 +0300

    playgroun_update

--- flask/flask_fundamentals/Playground/playground.py
+++ flask/flask_fundamentals/Playground/playground.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World!!"
 @app.route('/play')
-def boxes():
-    return render_template("index.html", box = "" ,times=0)
+def boxes( times=3):
+    return render_template("index.html" ,times =times)
 @app.route('/play/<num>')
 def number_boxes(num):
-    return render_template("index.html", box = "" , times =int(num)  )
+    return render_template("index.html" , times =int(num)  )
 @app.route('/play/<num>/<color>')
 def number_boxes_with_color(num , color):
-    return render_template("index.html", box = "" , times =int(num) , color = color )
+    return render_template("index.html", times =int(num) , color = color )
 
 
 
