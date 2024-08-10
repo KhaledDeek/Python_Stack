c0d961ed226801cf50e11f6bb0d75100cefb4d49
Deek <deek9517@gmail.com>
2024-08-11 00:58:22 +0300

    back

--- my_environments/Project/my_health/my_health/settings.py
+++ my_environments/Project/my_health/my_health/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q@qs@20#+&y!k5xtmt@ggi0q5hbl_+^bz(s7i2$v#r%ht8&_bv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,16 +74,14 @@ WSGI_APPLICATION = 'my_health.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-    'default' : dj_database_url.parse("postgresql://my_health_4ce2_user:1QYPZ4AFMtrje06xoVevz58wcMh1jZdI@dpg-cqrt6gdumphs73cqcv4g-a.oregon-postgres.render.com/my_health_4ce2"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
 #
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -121,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
