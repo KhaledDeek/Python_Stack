e63d39dbb8217f5d594ce84a5f4114ee53cb0a21
Deek <deek9517@gmail.com>
2024-08-11 11:49:23 +0300

    render

--- my_environments/Project/my_health/my_health/settings.py
+++ my_environments/Project/my_health/my_health/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os 
 import dj_database_url
 from pathlib import Path
 
@@ -74,13 +75,15 @@ WSGI_APPLICATION = 'my_health.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse("postgresql://my_health_rk8n_user:n234Bn4OqStN6JMuzX4z3AfQH5enXltL@dpg-cqs7gmogph6c73a80km0-a.oregon-postgres.render.com/my_health_rk8n")
 }
-
 
 #
 # Password validation
@@ -118,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
